38805b5305e217c2fc5647aefe12101b8b358979
Gustavo <guatarashi@gmail.com>
2023-03-25 15:41:40 -0300

    feat: Aula 3.

--- src/main.kt
+++ src/main.kt
@@ -3,6 +3,83 @@ package src
 fun main() {
     println("Bem vindo ao Bytebank")
 
+    val contaAlex = Conta()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 1000
+    contaAlex.saldo = 200.0
+
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
+
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+
+    println(contaAlex.titular)
+    println(contaAlex.numero)
+    println(contaAlex.saldo)
+
+    println("depositando na conta do Alex")
+    deposita(contaAlex, 50.0)
+    println(contaAlex.saldo)
+
+    println("depositando na conta do Fran")
+    deposita(contaFran, 70.0)
+    println(contaFran.saldo)
+}
+
+fun deposita(conta: Conta, valor: Double) {
+    conta.saldo += valor
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
+fun testaCopiasEReferencias() {
+    val numeroX = 1000
+    var numeroY = numeroX
+
+    numeroY++
+
+    println("número X $numeroX")
+    println("número Y $numeroY")
+
+    val contaJoao = Conta()
+    contaJoao.titular = "João"
+
+    var contaMaria = Conta()
+    contaMaria.titular = "Maria"
+    contaJoao.titular = "João"
+
+    println("titular conta joão: ${contaJoao.titular}")
+    println("titular conta maria: ${contaMaria.titular}")
+
+    println(contaJoao)
+    println(contaMaria)
+}
+
+fun testaCondicoes(saldo: Double) {
+    if (saldo > 0.0) {
+        println("conta é positiva")
+    } else if (saldo == 0.0) {
+        println("conta é neutra")
+    } else {
+        println("conta é negativa")
+    }
+
+    when {
+        saldo > 0.0 -> println("conta é positiva")
+        saldo == 0.0 -> println("conta é neutra")
+        else -> println("conta é negativa")
+    }
+}
+
+fun testaLaco() {
     var i = 0
     while (i < 5) {
         val titular: String = "Alex $i"
@@ -28,20 +105,4 @@ fun main() {
 //    }
 
 //    testaCondicoes(saldo)
-}
-
-fun testaCondicoes(saldo: Double) {
-    if (saldo > 0.0) {
-        println("conta é positiva")
-    } else if (saldo == 0.0) {
-        println("conta é neutra")
-    } else {
-        println("conta é negativa")
-    }
-
-    when {
-        saldo > 0.0 -> println("conta é positiva")
-        saldo == 0.0 -> println("conta é neutra")
-        else -> println("conta é negativa")
-    }
 }
