865fa151b83d94bef59273924f7c7ee9318d0099
Gustavo <guatarashi@gmail.com>
2023-03-25 17:15:01 -0300

    feat: Aula 4.

--- src/main.kt
+++ src/main.kt
@@ -6,38 +6,92 @@ fun main() {
     val contaAlex = Conta()
     contaAlex.titular = "Alex"
     contaAlex.numero = 1000
-    contaAlex.saldo = 200.0
+    contaAlex.setSaldo(200.0)
 
     val contaFran = Conta()
     contaFran.titular = "Fran"
     contaFran.numero = 1001
-    contaFran.saldo = 300.0
+    contaFran.setSaldo(300.0)
 
     println(contaFran.titular)
     println(contaFran.numero)
-    println(contaFran.saldo)
+    println(contaFran.getSaldo())
 
     println(contaAlex.titular)
     println(contaAlex.numero)
-    println(contaAlex.saldo)
+    println(contaAlex.getSaldo())
 
     println("depositando na conta do Alex")
-    deposita(contaAlex, 50.0)
-    println(contaAlex.saldo)
+    contaAlex.deposita(50.0)
+    println(contaAlex.getSaldo())
 
     println("depositando na conta do Fran")
-    deposita(contaFran, 70.0)
-    println(contaFran.saldo)
-}
+    contaFran.deposita(70.0)
+    println(contaFran.getSaldo())
 
-fun deposita(conta: Conta, valor: Double) {
-    conta.saldo += valor
+    println("sacando na conta do Alex")
+    contaAlex.saca(250.0)
+    println(contaAlex.getSaldo())
+
+    println("sacando na conta do Fran")
+    contaFran.saca(100.0)
+    println(contaFran.getSaldo())
+
+    println("Saque em excesso na conta do Alex")
+    contaAlex.saca(100.0)
+    println(contaAlex.getSaldo())
+
+    println("Saque em excesso na conta do Fran")
+    contaFran.saca(500.0)
+    println(contaFran.getSaldo())
+
+    println("Transferência da conta da Fran para o Alex")
+
+    if (contaFran.transfere(300.0, contaAlex)) {
+        println("Transferência sucedida")
+    } else {
+        println("Falha na transferência")
+    }
+
+    println(contaAlex.getSaldo())
+    println(contaFran.getSaldo())
 }
 
 class Conta {
     var titular = ""
     var numero = 0
-    var saldo = 0.0
+    private var saldo = 0.0
+
+    fun deposita(valor: Double) {
+        this.saldo += valor
+    }
+
+    fun saca(valor: Double) {
+        if (this.saldo >= valor) {
+            saldo -= valor
+        }
+    }
+
+    fun transfere(valor: Double, destino: Conta): Boolean {
+        if (saldo >= valor) {
+            saldo -= valor
+            destino.saldo -= valor
+
+            return true
+        }
+
+        return false
+    }
+
+    fun getSaldo(): Double {
+        return this.saldo
+    }
+
+    fun setSaldo(saldo: Double) {
+        if (saldo > 0) {
+            this.saldo = saldo
+        }
+    }
 }
 
 fun testaCopiasEReferencias() {
