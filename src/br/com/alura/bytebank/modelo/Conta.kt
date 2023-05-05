93543bb5ebd9b187b584f03a8788aa4e16b07b02
Gustavo <guatarashi@gmail.com>
2023-05-05 08:54:10 -0300

    feat: Aula 3 - Variáveis globais e objects

--- src/br/com/alura/bytebank/modelo/Conta.kt
+++ src/br/com/alura/bytebank/modelo/Conta.kt
@@ -6,6 +6,15 @@ abstract class Conta(
 ) {
     var saldo = 0.0
         protected set
+    companion object {
+        var total = 0
+            private set
+    }
+
+    init {
+        println("Criando conta")
+        total++
+    }
 
     fun deposita(valor: Double) {
         if (valor > 0) {
@@ -14,4 +23,35 @@ abstract class Conta(
     }
 
     abstract fun saca(valor: Double)
+}
+
+class ContaCorrente(
+    titular: Cliente,
+    numero: Int
+) : ContaTransferivel(
+    titular = titular,
+    numero = numero
+) {
+
+    override fun saca(valor: Double) {
+        val valorComTaxa = valor + 0.1
+        if (this.saldo >= valorComTaxa) {
+            this.saldo -= valorComTaxa
+        }
+    }
+}
+
+class ContaPoupanca(
+    titular: Cliente,
+    numero: Int
+) : ContaTransferivel(
+    titular = titular,
+    numero = numero
+){
+
+    override fun saca(valor: Double) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor
+        }
+    }
 }
