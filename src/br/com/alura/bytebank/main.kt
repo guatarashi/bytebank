13d10cd979d6896004e7d0cd17e9f19e9d90ae09
Gustavo <guatarashi@gmail.com>
2023-05-08 07:16:36 -0300

    feat: Aula 4 - Entendendo a Herança base do Kotlin.

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,33 +1,23 @@
 package src.br.com.alura.bytebank
 
-import src.br.com.alura.bytebank.modelo.Autenticavel
-import src.br.com.alura.bytebank.modelo.Cliente
-import src.br.com.alura.bytebank.modelo.Conta
-import src.br.com.alura.bytebank.modelo.ContaCorrente
-import src.br.com.alura.bytebank.modelo.ContaPoupanca
-import src.br.com.alura.bytebank.modelo.SistemaInterno
-import src.br.com.alura.bytebank.teste.testaContasDiferentes
+import src.br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
 
-    val fran = object : Autenticavel {
-        val nome: String = "Fran"
-        val cpf: String = "111.111.111-11"
-        val senha: Int = 1000
+    val endereco = Endereco()
 
-        override fun autentica(senha: Int) = this.senha == senha
-    }
+    println(endereco)
+//    println(Cliente())
 
-    val sistemaInterno = SistemaInterno()
-    sistemaInterno.entra(fran, 1000)
+    val objeto: Any = Any()
 
-    println("nome do cliente ${fran.nome}")
+    imprime(endereco)
 
-    val alex = Cliente(nome = "Alex", cpf = "", senha = 1)
-    val contaPoupanca = ContaPoupanca(titular = alex, numero = 1000)
-    val contaCorrente = ContaCorrente(titular = alex, numero = 1000)
+    val teste: Any = imprime(endereco)
+    println(teste)
+}
 
-    testaContasDiferentes()
-
-    println("Total de contas: ${Conta.total}")
+fun imprime(valor: Any): Any {
+    println(valor)
+    return valor
 }
---
+++ src/br/com/alura/bytebank/teste/testaObjects.kt
@@ -0,0 +1,31 @@
+package src.br.com.alura.bytebank.teste
+
+import src.br.com.alura.bytebank.modelo.Autenticavel
+import src.br.com.alura.bytebank.modelo.Cliente
+import src.br.com.alura.bytebank.modelo.Conta
+import src.br.com.alura.bytebank.modelo.ContaCorrente
+import src.br.com.alura.bytebank.modelo.ContaPoupanca
+import src.br.com.alura.bytebank.modelo.SistemaInterno
+
+fun testaObjects() {
+    val fran = object : Autenticavel {
+        val nome: String = "Fran"
+        val cpf: String = "111.111.111-11"
+        val senha: Int = 1000
+
+        override fun autentica(senha: Int) = this.senha == senha
+    }
+
+    val sistemaInterno = SistemaInterno()
+    sistemaInterno.entra(fran, 1000)
+
+    println("nome do cliente ${fran.nome}")
+
+    val alex = Cliente(nome = "Alex", cpf = "", senha = 1)
+    val contaPoupanca = ContaPoupanca(titular = alex, numero = 1000)
+    val contaCorrente = ContaCorrente(titular = alex, numero = 1000)
+
+    testaContasDiferentes()
+
+    println("Total de contas: ${Conta.total}")
+}
