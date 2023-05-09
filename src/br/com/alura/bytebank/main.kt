2929d4258c8e65c545b10b2d78e349b04228a2ce
Gustavo <guatarashi@gmail.com>
2023-05-09 07:24:44 -0300

    feat: Aula 5 - Reutilizando e sobrescrevendo métodos de any.

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -4,17 +4,27 @@ import src.br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
 
-    val endereco = Endereco()
+    val endereco = Endereco(
+        logradouro = "Rua Vergueiro",
+        complemento = "Alura",
+        cep = "000000-0700"
+    )
+    val enderecoNovo = Endereco(
+        bairro = "Vila Mariana",
+        numero = 1000,
+        cep = "000000-0700"
+    )
 
-    println(endereco)
-//    println(Cliente())
+    println(endereco.equals(endereco))
+    println(endereco.equals(enderecoNovo))
 
-    val objeto: Any = Any()
+    println(endereco.hashCode())
+    println(enderecoNovo.hashCode())
 
     imprime(endereco)
+    imprime(enderecoNovo)
 
-    val teste: Any = imprime(endereco)
-    println(teste)
+    println("${endereco.javaClass}@${Integer.toHexString(endereco.hashCode())}")
 }
 
 fun imprime(valor: Any): Any {
