f0dcd6f016a1e66cca586d0172c8ea7ea8022d9e
Gustavo <guatarashi@gmail.com>
2023-05-12 07:08:00 -0300

    feat: Conhecendo a pilha de execução.

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -4,30 +4,24 @@ import src.br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
 
-    val endereco = Endereco(
-        logradouro = "Rua Vergueiro",
-        complemento = "Alura",
-        cep = "000000-0700"
-    )
-    val enderecoNovo = Endereco(
-        bairro = "Vila Mariana",
-        numero = 1000,
-        cep = "000000-0700"
-    )
-
-    println(endereco.equals(endereco))
-    println(endereco.equals(enderecoNovo))
-
-    println(endereco.hashCode())
-    println(enderecoNovo.hashCode())
-
-    imprime(endereco)
-    imprime(enderecoNovo)
-
-    println("${endereco.javaClass}@${Integer.toHexString(endereco.hashCode())}")
+    println("inicio main")
+    10/0
+    funcao1()
+    println("fim main")
 }
 
-fun imprime(valor: Any): Any {
-    println(valor)
-    return valor
+fun funcao1() {
+    println("inicio funcao1")
+    funcao2()
+    println("fim funcao1")
+}
+
+fun funcao2() {
+    println("inicio funcao2")
+    for (i in 1..5) {
+        println(i)
+        val endereco = Any()
+        endereco as Endereco
+    }
+    println("fim funcao2")
 }
