8a6dc54878f672b47197e80849745dd8b95f1e7f
Gustavo <guatarashi@gmail.com>
2023-05-15 07:31:45 -0300

    feat: Aula 2.3 - Utilizando as expressões.

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -6,8 +6,41 @@ import src.br.com.alura.bytebank.modelo.Endereco
 fun main() {
 
     println("inicio main")
+
+    val entrada: String = "1.0"
+
+    val valorRecebido: Double? = try {
+        entrada.toDouble()
+    } catch (e: NumberFormatException) {
+        println("Problema na conversão")
+        e.printStackTrace()
+        null
+    }
+
+    val valorComTaxa: Double? = if (valorRecebido != null) {
+        valorRecebido + 0.1
+    } else {
+        null
+    }
+
+    // early return
+//    val valorComTaxa: Double? = valorComTaxa(100.0)
+
+    if (valorComTaxa != null) {
+        println("valor recebido: $valorComTaxa")
+    } else {
+        println("valor invalido")
+    }
+
     funcao1()
     println("fim main")
+}
+
+fun valorComTaxa(valorRecebido: Double?): Double? {
+    if(valorRecebido != null) {
+        return valorRecebido + 0.1
+    }
+    return null
 }
 
 fun funcao1() {
